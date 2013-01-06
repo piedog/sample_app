ed4627b03a3bd1a0752d2fb3b710f3a0438c0e19
Rob <rob@piepul.com>
2013-01-06 14:15:35 -0600

    Factored out title and h1 content

--- spec/requests/statics_spec.rb
+++ spec/requests/statics_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
+    let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
     describe "Home page" do
         it "should have the h1 'Sample App'" do
@@ -9,7 +10,7 @@ describe "Static pages" do
         end
         it "should have the title 'Home'" do
             visit '/static_pages/home'
-            page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+            page.should have_selector('title', :text => "#{base_title} | Home")
         end
     end
 
@@ -21,7 +22,7 @@ describe "Static pages" do
 
         it "should have the title 'Help'" do
             visit '/static_pages/help'
-            page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+            page.should have_selector('title', :text => "#{base_title} | Help")
         end
     end
 
@@ -33,8 +34,20 @@ describe "Static pages" do
 
         it "should have the title 'About'" do
             visit '/static_pages/about'
-            page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+            page.should have_selector('title', :text => "#{base_title} | About Us")
         end
     end
+
+    describe "Contact" do
+        it "should have the h1 'Contact Us'" do
+            visit '/static_pages/contact'
+            page.should have_selector('h1', :text => 'Contact Us')
+        end
+
+        it "should have the title 'Contact'" do
+            visit '/static_pages/contact'
+            page.should have_selector('title', :text => "#{base_title} | Contact")
+        end
+    end 
 
 end
