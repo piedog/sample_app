6cdd0b466773c37743654b7a5f1e3a80869d3654
Rob <rob@piepul.com>
2013-01-11 23:12:38 -0600

    Before changing paths in spec/requests/static_pages_spec.rb

--- spec/requests/statics_spec.rb
+++ spec/requests/statics_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
+
 describe "Static pages" do
     let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
@@ -43,15 +44,15 @@ describe "Static pages" do
         end
     end
 
-    describe "Contact" do
-        it "should have the h1 'Contact Us'" do
+    describe "Contact page" do
+        it "should have the h1 'Contact'" do
             visit '/static_pages/contact'
-            page.should have_selector('h1', :text => 'Contact Us')
+            page.should have_selector('h1', text: 'Contact')
         end
 
         it "should have the title 'Contact'" do
             visit '/static_pages/contact'
-            page.should have_selector('title', :text => "#{base_title} | Contact")
+            page.should have_selector('title', text: "Ruby on Rails Tutorial Sample App | Contact")
         end
     end 
 
