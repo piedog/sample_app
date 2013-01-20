2d1c5f9131ec937dfa62f211dc76fdbbd1aedb4e
Rob <rob@piepul.com>
2013-01-19 20:46:02 -0600

    8.1.5 Rendering with flash msg

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,13 @@ class SessionsController < ApplicationController
     end
 
     def create
+        user = User.find_by_email(params[:session][:password])
+        if user && user.authentication(params[:session][:password])
+            ## sign the user in and redirect to the user's sho page
+        else
+            flash.now[:error] = 'Invalid email/password combination'
+            render 'new'
+        end
     end
 
     def destroy
